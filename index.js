0b7dac666ae35e45cfc3b5c8b191b4f0f71684fc
futureliveparticeroom <145907111+futureliveparticeroom@users.noreply.github.com>
2023-09-28 05:38:36 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
+const fetch = require('cross-fetch');
 
 // 设置模板引擎
 app.set('view engine', 'ejs');
@@ -9,31 +10,32 @@ app.set('view engine', 'ejs');
 // 设置静态文件夹
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.get('/home', (req, res) => {
-    const videosDir = path.join(__dirname, 'public', 'videos'); // 视频文件夹路径
-    fs.readdir(videosDir, (err, files) => {
-        if (err) return res.status(500).send('Error reading videos directory');
-        // 只获取前10个视频文件
-        const videos = files.slice(0, 4).map(file => `/public/videos/${file}`);
-        res.render('index', { videos });
-    });
-});
+app.get('/', async (req, res) => {
+    try {
+      const response = await fetch('http://165.227.122.172:3002/get-video-paths');
+      const video = await response.json();
+  
+      res.render('index', { video });
+    } catch (error) {
+      console.error('Error fetching video paths:', error);
+      res.status(500).send('Error fetching video paths');
+    }
+  });
 
-app.get('/get-latest-videos', (req, res) => {
-    const videosDir = path.join(__dirname, 'public', 'videos'); // 视频文件夹路径
-    fs.readdir(videosDir, (err, files) => {
-        if (err) return res.status(500).send('Error reading videos directory');
-
-     
-        const videos = files.slice(0, 4).map(file => `/public/videos/${file}`);
-
-        res.json(videos);
-    });
-});
-
+app.get('/get-latest-videos',async (req, res) => {
+    try {
+      const response = await fetch('http://165.227.122.172:3002/get-video-paths'); // 替换为3002端口应用上正确的路径
+      const video = await response.json();
+      res.json(video);
+    } catch (error) {
+      console.error('Error fetching latest videos:', error);
+      res.status(500).send('Error fetching latest videos');
+    }
+  });
 
 
-const PORT = 3000;
+
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
